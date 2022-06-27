15eb306df479187ec260b1fa516cf672e3edc8f1
Gan Nguyen <gannguyen@Gans-MacBook-Pro.local>
2022-06-27 16:35:08 +0800

    function

--- src/Animal.java
+++ src/Animal.java
@@ -2,11 +2,21 @@ public class Animal {
     // properties : thuộc tính hiển thị thông tin đối tượng
     float weight;
     float height;
+    boolean isHerbivore;
 
     // constructor: phương thức khởi tạo
-    public Animal(float weight, float height){
+    public Animal(float weight, float height, boolean isHerbivore){
         this.weight = weight;
         this.height =  height;
+        this.isHerbivore = isHerbivore;
     }
 
+    // function : hành vi
+    public void eat() {
+        if (isHerbivore){
+            System.out.println("Động vật ăn cỏ");
+        } else {
+            System.out.println("Động vật ăn thit");
+        }
+    }
 }
